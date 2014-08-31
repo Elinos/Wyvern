93da33d6f92351e84bea1ebaa72c397b866fface
Kiril Tsarnakliyski <elinos@gmail.com>
2014-08-31 18:56:57 +0300

    Data Access

---
+++ CoffeeCompany.DataAccess.DataManager/DataAccessManager.cs
@@ -0,0 +1,18 @@
+﻿using CoffeeCompany.DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeCompany.DataAccess.DataManager
+{
+    public class DataAccessManager
+    {
+        public void AddReport()
+        {
+            var mySqlDb = new MySQLEntitiesModel();
+            Reports newReport = new Reports();
+        }
+    }
+}
