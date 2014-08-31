68998e31787f94f103ff14cedff1e0788d2ba8ef
Kiril Tsarnakliyski <elinos@gmail.com>
2014-08-31 16:47:00 +0300

    final revert!

--- CoffeeCompany.ConsoleClient/CoffeeCompanyConsoleClient.cs
+++ CoffeeCompany.ConsoleClient/CoffeeCompanyConsoleClient.cs
@@ -1,26 +1,10 @@
 ﻿namespace CoffeeCompany.ConsoleClient
 {
     using System;
-    using System.Linq;
-
-    using CoffeeCompany.Data;
-    using CoffeeCompany.Models;
     class CoffeeCompanyConsoleClient
     {
         static void Main(string[] args)
         {
-            var context = new CoffeeCompanyDbContext();
-
-            var company = new ClientCompany()
-            {
-                Name = "AVG"
-            };
-
-            context.Companies.Add(company);
-            context.SaveChanges();
-
-            var savedCompany= context.Companies.First();
-            Console.WriteLine("ID {0}: {1}", savedCompany.ID, savedCompany.Name);
         }
     }
 }
--- CoffeeCompany.Models/Orders.cs
+++ CoffeeCompany.Models/Orders.cs
@@ -2,7 +2,6 @@
 {
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
 
     public class Order
     {
@@ -13,17 +12,13 @@
             this.products = new HashSet<Product>();
         }
         public int ID { get; set; }
-
-        [Required]
         public int QuantityInKg { get; set; }
 
-        [NotMapped]
+        // In dollars
         public decimal OrderTotalPrice { get; set; }
 
-        [Required]
         public OrderStatus Status { get; set; }
 
-        [Required]
         public virtual ICollection<Product> Products
         {
             get
@@ -35,9 +30,5 @@
                 this.products = value;
             }
         }
-
-        public int ClientCompanyId { get; set; }
-
-        public virtual ClientCompany ClientCompany { get; set; }
     }
 }
