5706fa53223b96ec0b017db1b0fa57d501a83fa3
Umahizi <iskra_i_popova@mail.bg>
2014-09-03 14:08:51 +0300

    Xml-reporter

---
+++ CoffeeCompany/CoffeeCompany.ReportGenerator/XMLExporter.cs
@@ -0,0 +1,37 @@
+﻿namespace CoffeeCompany.ReportGenerator
+{
+    using System.Collections.Generic;
+    using System.Xml;
+
+    public class XMLExporter
+    {
+        public XmlDocument document;
+
+        public XMLExporter()
+        {
+            this.document = new XmlDocument();
+        }
+
+        public void ExportDocument(List<List<string>> content, string title, List<string> elements, string path)
+        {
+            XmlElement outerElement = document.CreateElement("Report");
+
+            outerElement.SetAttribute("title", null, title);
+
+            foreach (var set in content)
+            {
+                for (int i = 0; i < elements.Count; i++)
+                {
+                    XmlElement element = document.CreateElement(elements[i]);
+                    element.InnerText = set[i];
+                    outerElement.AppendChild(element);
+
+                }
+            }
+
+            this.document.Save(path);
+        }
+
+    }
+}
+
