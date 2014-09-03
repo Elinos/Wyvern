8a8430bca496cbdb5795b19b0ba4c507608e04aa
ADimanova <addi_91@abv.bg>
2014-09-03 16:13:55 +0300

    XML Reporter

--- CoffeeCompany/CoffeeCompany.ReportGenerator/XMLExporter.cs
+++ CoffeeCompany/CoffeeCompany.ReportGenerator/XMLExporter.cs
@@ -7,26 +7,41 @@
     {
         public XmlDocument document;
 
-        public XMLExporter()
+        private string StringConcatenatior(string rawString)
         {
-            this.document = new XmlDocument();
+            var parts = rawString.Split(' ');
+            var parsedString = string.Join("", parts);
+
+            return parsedString;
         }
 
         public void ExportDocument(List<List<string>> content, string title, List<string> elements, string path)
         {
+            this.document = new XmlDocument();
+            document.LoadXml("<?xml version='1.0' ?>" +
+    "<ProductsReports>" +
+    "</ProductsReports>");
+
+            XmlNode root = document.DocumentElement;
             XmlElement outerElement = document.CreateElement("Report");
 
-            outerElement.SetAttribute("title", null, title);
+            var parsedTitle = StringConcatenatior(title);
+            outerElement.SetAttribute("title", parsedTitle);
+            outerElement.Prefix = "";
+            root.AppendChild(outerElement);
 
             foreach (var set in content)
             {
+                XmlElement productInfo = document.CreateElement("ProductInfo");
                 for (int i = 0; i < elements.Count; i++)
                 {
-                    XmlElement element = document.CreateElement(elements[i]);
+                    var parsedElement = StringConcatenatior(elements[i]);
+                    XmlElement element = document.CreateElement(parsedElement);
                     element.InnerText = set[i];
-                    outerElement.AppendChild(element);
-
+                    productInfo.AppendChild(element);
                 }
+
+                outerElement.AppendChild(productInfo);
             }
 
             this.document.Save(path);
