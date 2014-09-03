1d39ddd2a1016edce94548333c141d59f0ebec34
Umahizi <iskra_i_popova@mail.bg>
2014-09-03 16:54:22 +0300

    ConsoleRenderer

---
+++ CoffeeCompany/CoffeeCompany.ConsoleClient/Renderer/ConsoleRenderer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeCompany.ConsoleClient.Renderer
+{
+    public class ConsoleRenderer
+    {
+
+        public ConsoleRenderer() 
+        {
+            this.Legend = new Legend();
+        }
+
+        internal Legend Legend { get; set; }
+
+        public void PrintLegend()
+        {
+            Console.Clear();
+            Console.WriteLine(Legend.comandLegend);
+        }
+
+        public void PrintExportLegend()
+        {
+            Console.Clear();
+            Console.WriteLine(Legend.ExportLegend);
+        }
+
+        public void PrintLoadLegend()
+        {
+            Console.Clear();
+            Console.WriteLine(Legend.LoadLegend);
+        }
+
+
+    }
+}
---
+++ CoffeeCompany/CoffeeCompany.ConsoleClient/Renderer/Legend.cs
@@ -0,0 +1,42 @@
+﻿namespace CoffeeCompany.ConsoleClient.Renderer
+{
+    using System;
+
+    class Legend
+    {
+        public const string comandLegend =
+            "-----------------Command Legend------------------" +
+            "\n" +
+            "\n" +
+            "\n" +
+            "\n To export reports type *Export*" +
+            "\n" +
+            "\n To load files to the database type *Load*";
+
+        public const string ExportLegend =
+            "Welcome to the export interface" +
+            "\n" +
+            "\n-----to export as Json Object type *Json*" +
+            "\n" +
+            "\n-----to export as Xml docummnet type *Xml*" +
+            "\n" +
+            "\n-----to export a Pdf report type *Pdf*" +
+            "\n" +
+            "\n-----to export Excel report type *Excel*"+
+             "\n" +
+            "\n-----to go back type *Back*";
+
+        public const string LoadLegend =
+            "Welcom to the Loading interface" +
+            "\n" +
+            "\n------to load from Excel file type *Excel*" +
+            "\n" +
+            "\n------to load from MongoDB database type *Mongo*" +
+            "\n" +
+            "\n------to load from XML file type *Xml*" +
+             "\n" +
+            "\n-----to go back type *Back*";
+
+        public const string ExecutingCommand = "Executing Command";
+    }
+}
