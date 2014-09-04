8405c4506e25f0b60a4b300769d5fc940d7fb871
Kiril Tsarnakliyski <elinos@gmail.com>
2014-09-04 13:45:56 +0300

    UI Client

---
+++ CoffeeCompany/CoffeeCompany.UI.Client/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using CoffeeCompany.Excel.Manager;
+using CoffeeCompany.Import;
+using CoffeeCompany.MySQL.Manager;
+
+namespace CoffeeCompany.UI.Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private DataImport dataImport = new DataImport();
+
+
+        private void FromZipFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            dataImport.ImportFromExcel();
+        }
+
+        private void ToExcelFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            var excelManager = new ExcelManager();
+            excelManager.CreateExcelReport();
+        }
+
+        private void FromMongoDBButton_Click(object sender, RoutedEventArgs e)
+        {
+            dataImport.ImportFromMongoDb();
+        }
+
+        private void ToPDFButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToJSONButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ToXMLButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FromXMLButton_Click(object sender, RoutedEventArgs e)
+        {
+            dataImport.ImportFromXml();
+        }
+
+        private void ToMySQLButton_Click(object sender, RoutedEventArgs e)
+        {
+            var mySqlManager = new MySQLManager();
+            mySqlManager.LoadAllReportsDataFromSQLServer();
+        }
+    }
+}
